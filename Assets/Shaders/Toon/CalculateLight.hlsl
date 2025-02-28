4fc5c8fc65dd34f323643ceaaba5c998184e07b1
NAwakening <robertogarzajoa@gmail.com>
2025-02-28 16:04:25 -0600

    toon final

--- Assets/Shaders/Toon/CalculateLight.hlsl
+++ Assets/Shaders/Toon/CalculateLight.hlsl
@@ -2,15 +2,25 @@
 #define CUSTOM_LIGHTING_INCLUDED
 
 // Obtiene una posicion en el mundo y nos regresa una direccion y color de la luz de unity
-void CalculateLight_float(float3 WorldPos, out float3 Direction, out float3 Color)
+void CalculateLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out half DistanceAtten, out half ShadowAtten)
 {
     #if SHADERGRAPH_PREVIEW
     Direction = float3(0.5f,0.5f,0);
     Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
     #else
-    Light mainLight = GetMainLight(0);
+    #if SHADOW_SCREEN
+        half4 clipPos = TransformWorldToShadowCoord(WorldPos)
+        half4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+    Light mainLight = GetMainLight(shadowCoord);
     Direction = mainLight.direction;
     Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+    ShadowAtten = mainLight.shadowAttenuation;
     #endif
 }
 #endif
